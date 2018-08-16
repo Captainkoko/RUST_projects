0384891180fbb8655b9f1383a8a79ac1b170d379
CaptainKoKo <41950999+Captainkoko@users.noreply.github.com>
2018-08-16 17:15:18 +0700

    test

---
+++ rectangles/target/debug/deps/rectangles-3079d5636a0110e4.d
@@ -0,0 +1,5 @@
+C:\Users\JACKIE PHAM\projects\rectangles\target\debug\deps\rectangles-3079d5636a0110e4.exe: src\main.rs
+
+C:\Users\JACKIE PHAM\projects\rectangles\target\debug\deps\rectangles-3079d5636a0110e4.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\JACKIE\ PHAM\projects\rectangles\target\debug\rectangles.pdb: C:\Users\JACKIE\ PHAM\projects\rectangles\src\main.rs
